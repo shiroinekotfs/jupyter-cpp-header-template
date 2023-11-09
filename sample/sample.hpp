7c979d10908c5a99a0e252bfe0f73678314a70d9
白い猫 <shiroineko.tfs@gmail.com>
2023-11-09 07:13:16 +0700

    Update sample.hpp

--- sample/sample.hpp
+++ sample/sample.hpp
@@ -11,18 +11,18 @@ extern "C" {
 
 namespace _CPP_KERNEL_HEADER_TEST {
     void TestHeaderMode() {
-        std::cout << "External headers are working properly." << std::endl;
+        std::cout << "External headers are working properly.\n\n" << std::endl;
 
         #ifdef _CPP_KERNEL_HEADER_TEST_legacy
-            printf("C Header Legacy is working as expected.");
+            printf("C Header Legacy is working as expected.\n\n");
         #else
-            printf("C Header Legacy is NOT working as expected.");
+            printf("C Header Legacy is NOT working as expected.\n\n");
         #endif
     
         #ifdef _CPP_KERNEL_HEADER_TEST_linker
-            std::cout << "C++ Linker is working as expected." << std::endl;
+            std::cout << "C++ Linker is working as expected.\n\n" << std::endl;
         #else
-            std::cout << "C Linker is NOT working as expected." << std::endl;
+            std::cout << "C Linker is NOT working as expected.\n\n" << std::endl;
         #endif
     }
 }
